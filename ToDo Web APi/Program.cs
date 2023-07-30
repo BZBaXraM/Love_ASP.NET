31bcf112db822d704830942d1dd422e12ffdfdce
Бахрам Байрамзаде <baxram1997007@gmail.com>
2023-07-30 20:58:50 +0400

    Added JWT token

--- ToDo Web APi/Program.cs
+++ ToDo Web APi/Program.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using ToDo_Web_APi.Data;
 using ToDo_Web_APi.Services;
@@ -10,6 +12,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters =
+            new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ClockSkew = TimeSpan.Zero,
+                ValidIssuer = "https://localhost:7137",
+                ValidAudience = "https://localhost:7137",
+                IssuerSigningKey =
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Super Hard Secure Key"))
+            };
+    });
 builder.Services.AddSwaggerGen(setup =>
 {
     setup.SwaggerDoc("v1"
@@ -19,6 +38,31 @@ builder.Services.AddSwaggerGen(setup =>
             Version = "v1",
         });
     setup.IncludeXmlComments(@"bin/Debug/net6.0/ToDo Web APi.xml");
+
+    setup.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "example: Bearer sowojsmajiqjeja"
+    });
+
+    setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer",
+                }
+            },
+            new string[] { }
+        }
+    });
 });
 builder.Services.AddScoped<IAsyncToDoService, ToDoService>();
 builder.Services.AddDbContext<ToDoDbContext>(options => options.UseNpgsql(
--- ToDo Web APi/Services/IAsyncToDoService.cs
+++ ToDo Web APi/Services/IAsyncToDoService.cs
@@ -3,14 +3,41 @@ using ToDo_WEB_API.DTOs.Pagination;
 
 namespace ToDo_Web_APi.Services;
 
+/// <summary>
+/// 
+/// </summary>
 public interface IAsyncToDoService
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="page"></param>
+    /// <param name="pageSize"></param>
+    /// <param name="search"></param>
+    /// <param name="isCompleted"></param>
+    /// <returns></returns>
     Task<PaginationListDto<ToDoItemDto>> GetToDoItemsAsync(int page,
         int pageSize,
         string? search,
         bool? isCompleted);
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     Task<ToDoItemDto> GetToDoItemAsync(int id);
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="isCompleted"></param>
+    /// <returns></returns>
     Task<ToDoItemDto> ChangeTodoItemStatusAsync(int id, bool isCompleted);
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
     Task<ToDoItemDto> CreateToDoItemAsync(CreateToDoItemRequest request);
 }
